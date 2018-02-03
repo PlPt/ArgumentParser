30ef0b4f29fbbd71703cf1e3f2e3e4e737e008bc
Pascal Petzoldt <p.petzoldt@web.de>
2018-02-03 23:57:58 +0100

    Implmented ListTest

--- src/de/plpt/ArgumentParserTest/ArgumentObject.java
+++ src/de/plpt/ArgumentParserTest/ArgumentObject.java
@@ -3,29 +3,38 @@ package de.plpt.ArgumentParserTest;
 import de.plpt.ArgumentParser.CommandInfo;
 import de.plpt.ArgumentParser.ParameterInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArgumentObject {
 
-    private boolean quit =false;
-    public ArgumentObject(){
+    private boolean quit = false;
+    List<String> list = new ArrayList<>();
+    public ArgumentObject() {
     }
 
 
     @CommandInfo(command = "add-item ([^;\\n]+);([^;\\n]+);(\\d{2})")
-    public String processGetItem(String name,float id,@ParameterInfo(minValue  = 55,maxValue = 65) int z){
+    public String processGetItem(String name, float id, @ParameterInfo(minValue = 55, maxValue = 65) int z) {
         return "name" + " " + id;
     }
 
-    @CommandInfo(command = "print ([^;\\n]+)")
-    public String print(String name){
-        return "Hello " + name;
+    @CommandInfo(command = "put ([^;\\n]+)")
+    public void put(String name) {
+       list.add(name);
+    }
+
+    @CommandInfo(command = "print")
+    public String print() {
+        return String.join("; ",list);
     }
 
     @CommandInfo(command = "quit")
-    public void quit(){
+    public void quit() {
         this.quit = true;
     }
 
-    public boolean getQuit(){
+    public boolean getQuit() {
         return quit;
     }
 }
